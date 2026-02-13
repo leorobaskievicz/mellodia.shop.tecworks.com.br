b442987aba6cc3fae9977d65cb9268eaca44d149
Leonardo Robaskievicz <leonardo@tecworks.com.br>
2026-02-13 10:37:22 -0300

    Não há comentário para esse commit

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -92,11 +92,11 @@ export default function RootLayout({ children }) {
         <link rel="preload" href="/favicon/favicon-96x96.png" as="image" type="image/png" />
         <link rel="preload" href="/favicon/favicon.svg" as="image" type="image/svg+xml" />
 
-        <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
-        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
-        <link rel="shortcut icon" href="/favicon/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
